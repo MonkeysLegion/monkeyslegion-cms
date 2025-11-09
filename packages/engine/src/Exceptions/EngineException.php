2ff116105d882a1932b8e277b08c619267351df5
Amanar Marouane <marouane.amanar07@gmail.com>
2025-11-09 14:49:23 +0100

    chore: Engine and ModuleNotFound exceptions

---
+++ packages/engine/src/Exceptions/EngineException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonkeysLegion\Engine\Exceptions;
+
+use RuntimeException;
+
+class EngineException extends RuntimeException
+{
+    // TODO: add logging later to the default behavior
+}
---
+++ packages/engine/src/Exceptions/ModuleNotFoundException.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonkeysLegion\Engine\Exceptions;
+
+class ModuleNotFoundException extends EngineException {}
